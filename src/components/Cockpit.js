ade6781117f23635daed9f9f47c14b52afb5bef2
Thomas <thomas.lachieze@gmail.com>
2020-04-29 19:25:15 +0200

    First commit

---
+++ src/components/Cockpit.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Cockpit extends React.Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        <>
+        <section>
+            <div className="Cockpit">
+                
+            </div>
+        </section>
+        </>
+    }
+}
+
+export default Cockpit;
