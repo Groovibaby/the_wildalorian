d503f9b448f3c57474bf8a823d9afd76dadb2fe8
GRAND <bengrand.pro@gmail.com>
2020-04-29 22:05:00 +0200

    add yodaSentence

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+
+
+
+
+
+
+
+
 import YodaSentence from './components/YodaSentence';
 
 function App() {
   return (
     <div className="App">
-     <YodaSentence />
+
+
+
+
+      <YodaSentence />
     </div>
   );
 }
