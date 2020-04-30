1f0cb2ef933fde16ad80089e0ee8e61bdb2a70da
GRAND <bengrand.pro@gmail.com>
2020-04-30 11:59:49 +0200

    add style again

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,10 +10,10 @@ class Footer extends React.Component {
                     <img src= {logoPng} alt="The Wildalorian" className='img-logo'/>
                 </div>
                 <div className='footer-link'>
-                    <a href="#">The flight Crew</a>
-                    <a href="#">Travel!</a>
-                    <a href="#">Réalisé par Thomas, François, Michaël et Benoit</a>
-                    <a href="#">Wild Code School</a>
+                    <a href="#" className= "footer-link2">The flight Crew</a>
+                    <a href="#"className= "footer-link2">Travel!</a>
+                    <a href="#"className= "footer-link2">Réalisé par Thomas, François, Michaël et Benoit</a>
+                    <a href="#"className= "footer-link2">Wild Code School</a>
                 </div>
             </div>
         )
